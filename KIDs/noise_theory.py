cfbfe49960b085580c50f046f65d3c5bda6d40f4
Wheeler1711 <Wheeler1711@gmail.com>
2017-10-12 14:23:40 -0600

    Updated to include responsivity/noise calculations using n*

--- KIDs/noise_theory.py
+++ KIDs/noise_theory.py
@@ -51,6 +51,8 @@ def df_response(t,tc,f):
 
 
 def grnoise(t,tc,V,tau_qp,N0,f,nqp_min):
+# this function calculates gr noise assuming constant tau
+#fuction below responsivity probably does a better job. 
 # V in cubic microns
 #if ~keyword_set(N0) then N0=4.e10 ; microns^3 / eV
 #if ~keyword_set(tau_qp) then tau_qp = 5e-6 ; sec
@@ -72,3 +74,77 @@ def grnoise(t,tc,V,tau_qp,N0,f,nqp_min):
 
     ef2 = 4. * beta**2 * nqp(t,tc,V,nqp_min) * tau_qp
     return ef2
+
+
+def responsivity(temp,pabs,tc = 1.,N0 = 4.*10**10,nstar =100.,tau_max = 100.,eta_pb = 0.7,vol = 1.,fr = 100.,alpha_k = 1.,gamma_t = 1.,nu_opt = 250, n_gamma = 0.):
+    # specail thanks to Steve Hailey Dunsheath whose made this orginal function in idl 
+    #Define various constants 
+    k_B = 1.381*10**-23 #Boltzmann constant [J K^-1]
+    ev_joule = 1.6022*10**-19 #eV/J ratio [eV J^-1]
+    h_p = 6.626*10**-34 #Planck constant [J s]
+
+    #Compute the ratio of Delta_0/k_B [K]
+    d0_kB = 1.764*tc
+
+    # Compute n thermal
+    nth = 2.*N0*k_B/ev_joule*np.sqrt(2.*np.pi*temp*d0_kB)*np.exp(-1.*d0_kB/temp)
+
+    # Compute nqp
+    # This expression has a term of the form [sqrt(1 + eps) - 1], where eps is small when nth and pabs are small.
+    # When eps is small this term is not computed accurately, and here we explicitly linearize it.
+    #nqp = nstar*(-1. + sqrt(1. + 2.*nth/nstar + (nth/nstar)^2. + 2.*eta_pb*pabs*1.e-12*tau_max*1.e-6/(nstar*vol*d0_kB*k_B)))
+    eps = 2.*nth/nstar + (nth/nstar)**2. + 2.*eta_pb*pabs*1.*10**-12*tau_max*1.*10**-6/(nstar*vol*d0_kB*k_B)
+    term = np.sqrt(1. + eps) - 1.
+    indx = np.where(eps < 1*10**-8)
+    count = len(indx)
+    if (count != 0):
+        term[indx] = 0.5*eps[indx]
+    nqp = nstar*term
+    
+    #Compute tau_qp
+    tau_qp = tau_max/(1. + nqp/nstar)
+
+    # Compute S1 and S2
+    xi = h_p*fr*1*10**6/(2.*k_B*temp)
+    s1 = (2./np.pi)*np.sqrt(2.*d0_kB/(np.pi*temp))*np.sinh(xi)*special.iv(0,xi)
+    s2 = 1. + np.sqrt(2.*d0_kB/(np.pi*temp))*np.exp(-1.*xi)**special.iv(0,xi)
+    #s2 = 3.
+
+    #Compute xr and Qi_inv
+    #Note that xr refers to the frequency shift from the nqp = 0 state
+    xr = -1.*alpha_k*gamma_t*s2*nqp/(4.*N0*d0_kB*k_B/ev_joule)
+    Qi_inv = -1.*xr*2.*s1/s2
+
+    #Compute the frequency and Qinv responsivity
+    r_x = -1.*alpha_k*gamma_t*s2/(4.*N0*d0_kB*k_B/ev_joule)*eta_pb*tau_qp*1*10**-6/(d0_kB*k_B*vol)
+    r_qinv = -1.*r_x*2.*s1/s2
+
+    #Compute Sxx_gr and Sxx_gr0
+    tau_th = tau_max/(1. + nth/nstar) #quasiparticle lifetime for a superconductor in thermal equilibrium at the specified temperature [microsec]
+    gamma_th = nth*vol/2.*(1./tau_max + 1./tau_th)*1*10**6 #quasiparticle generation rate due to thermal fluctuations ;[sec^-1]
+    gamma_r = nqp*vol/2.*(1./tau_max + 1./tau_qp)*1*10**6 #quasiparticle recombination rate ;[sec^-1]
+    sxx_gr = (alpha_k*gamma_t*s2/(4.*N0*d0_kB*k_B/ev_joule))**2.*4.*(tau_qp*1*10**-6)**2./vol**2.*(gamma_th + gamma_r)
+    sxx_gr0 = (alpha_k*gamma_t*s2/(4.*N0*d0_kB*k_B/ev_joule))**2.*4.*nqp/vol*tau_qp*1*10**-6
+
+    #Compute Sxx_gamma
+    sxx_gamma = (r_x)**2.*2.*h_p*nu_opt*1*10**9*pabs*1*10**-12*(1. + n_gamma)
+
+    #Define the output dictionary and return
+    dict = {'nth':nth, 
+          'nqp':nqp, 
+          'tau_qp':tau_qp, 
+          's1':s1, 
+          's2':s2, 
+          'xr':xr, 
+          'Qi_inv':Qi_inv, 
+          'r_x':r_x, 
+          'r_qinv':r_qinv, 
+          'sxx_gr':sxx_gr, 
+          'sxx_gr0':sxx_gr0, 
+          'sxx_gamma':sxx_gamma}
+        
+    return dict
+
+def responsivity_help():
+    print "The input variables are temp,pabs,tc = 1.,N0 = 4.*10**10,nstar =100.,tau_max = 100.,eta_pb = 0.7,vol = 1.,fr = 100.,alpha_k = 1.,gamma_t = 1.,nu_opt = 250, n_gamma = 0."
+    print "The output variables are nth, nqp, tau_qp, s1, s2, xr, Qi_inv, r_x, r_qinv, sxx_gr, sxx_gr0, sxx_gamma"
