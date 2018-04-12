6a10a42f43ecc4ba6faf6c566f46df8b5af343f0
Wheeler1711 <Wheeler1711@gmail.com>
2018-04-12 14:30:27 -0600

    made compatiable with python 3
    Added in some parenthesis to print statements so that this
    can be run in python3 since python2 is soon to be dead

--- lab_brick/labbrick_startup.py
+++ lab_brick/labbrick_startup.py
@@ -10,7 +10,7 @@ attn1 = core.Attenuator(0x041f,0x1208,"15915")
 attn2 = core.Attenuator(0x041f,0x1208,"16776")
 
 
-print "15915 is attn1"
-print "16776 is attn2"
-print "example command: attn1.get_attenuation()"
-print "example command: attn2.set_attenuation(29)"
+print("15915 is attn1")
+print("16776 is attn2")
+print("example command: attn1.get_attenuation()")
+print("example command: attn2.set_attenuation(29)")
