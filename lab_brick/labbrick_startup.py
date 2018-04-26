74e933f42a2e92bd0c3406123061bf6d3024087c
Joseph Redford <jredford@caltech.edu>
2018-04-26 09:26:48 -0700

    modified serial number for caltech

--- lab_brick/labbrick_startup.py
+++ lab_brick/labbrick_startup.py
@@ -6,8 +6,8 @@ from lab_brick import core
 #execfile("labbrick_startup.py")
 
 #attn2 = core.Attenuator(0x041f,0x1208,"16776")
-attn1 = core.Attenuator(0x041f,0x1208,"15915")
-attn2 = core.Attenuator(0x041f,0x1208,"16776")
+#attn1 = core.Attenuator(0x041f,0x1208,"15915")
+attn2 = core.Attenuator(0x041f,0x1208,"01784")
 
 
 print "15915 is attn1"
