ea4702b1d3bf13fa96b6000c7aca270cfdd353de
Wheeler1711 <Wheeler1711@gmail.com>
2018-08-23 13:51:44 -0600

    fit_fine_gain now flags data near other resonators
    I added in not using the data that near other resonators
    when for fitting. for the fine scan data it only uses the
    data halfway inbetween two resonators if they are very close
    to each other. For the gain data it does not use any
    frequency points that are within one fine scan span of a
    resonator

--- multitone_kidPy/analyze.py
+++ multitone_kidPy/analyze.py
@@ -16,138 +16,173 @@ from KIDs import PCA
 # this function fits a fine and gain scan combo produced by the ASU multitone system
 def fit_fine_gain(fine_name,gain_name):
         
-	fine = read_multitone.read_iq_sweep(fine_name)
-	gain = read_multitone.read_iq_sweep(gain_name)
-	outfile_dir = fine_name
-
-	pdf_pages = PdfPages(outfile_dir+"/"+"fit_plots.pdf")
-
-	all_fits_mag = np.zeros((8,fine['freqs'].shape[1]))
-	all_fits_iq = np.zeros((9,fine['freqs'].shape[1]))
-
-	for i in range(0,fine['freqs'].shape[1]):
-		fine_f = fine['freqs'][:,i]*10**6
-		gain_f = gain['freqs'][:,i]*10**6
-		fine_z = fine['I'][:,i]+1.j*fine['Q'][:,i]
-		gain_z = gain['I'][:,i]+1.j*gain['Q'][:,i]
-
-		fig = plt.figure(i,figsize = (12,12))
-
-		plt.subplot(221)
-		plt.title("Resonator Index "+str(i))
-		plt.plot(fine['freqs'][:,i],10*np.log10(fine['I'][:,i]**2+fine['Q'][:,i]**2),'o',label = "gain")
-		plt.plot(gain['freqs'][:,i],10*np.log10(gain['I'][:,i]**2+gain['Q'][:,i]**2),'o',label = "fine")
-		plt.xlabel("Frequency (MHz)")
-		plt.ylabel("Power (dB)")
-
-		plt.subplot(223)
-		plt.plot(fine['freqs'][:,i],10*np.log10(fine['I'][:,i]**2+fine['Q'][:,i]**2),'o')
-		plt.plot(gain['freqs'][:,i],10*np.log10(gain['I'][:,i]**2+gain['Q'][:,i]**2),'o')
-		plt.xlabel("Frequency (MHz)")
-		plt.ylabel("Power (dB)")
-		plt.xlim(np.min(fine['freqs'][:,i]),np.max(fine['freqs'][:,i]))
-
-		# fit nonlinear magnitude
-		try:
-			x0 = resonance_fitting.guess_x0_mag_nonlinear_sep(fine_f,fine_z,gain_f,gain_z,verbose = True)
-			fit_dict_mag = resonance_fitting.fit_nonlinear_mag_sep(fine_f,fine_z,gain_f,gain_z,x0=x0)#,bounds =bounds)
-			all_fits_mag[:,i] = fit_dict_mag['fit'][0]
-			plt.subplot(221)
-			plt.plot(fit_dict_mag['fit_freqs']/10**6,10*np.log10(fit_dict_mag['fit_result']),"+",label = "fit")
-			plt.plot(fit_dict_mag['fit_freqs']/10**6,10*np.log10(fit_dict_mag['x0_result']),"x",label = "x0 guess")
-			plt.legend()
-			plt.subplot(223)
-			plt.plot(fit_dict_mag['fit_freqs']/10**6,10*np.log10(fit_dict_mag['fit_result']),"+")
-			plt.plot(fit_dict_mag['fit_freqs']/10**6,10*np.log10(fit_dict_mag['x0_result']),"x")
-		except Exception as e:
-			print(e)
-			print("could not fit the resonator")
+    fine = read_multitone.read_iq_sweep(fine_name)
+    gain = read_multitone.read_iq_sweep(gain_name)
+    outfile_dir = fine_name
+    center_freqs = fine['freqs'][fine['freqs'].shape[0]//2,:]
 
 
-		plt.subplot(222,aspect ='equal')
-		plt.plot(fine['I'][:,i],fine['Q'][:,i],'o')
-		plt.plot(gain['I'][:,i],gain['Q'][:,i],'o')
-		plt.xlabel("I")
-		plt.ylabel("Q")
+    pdf_pages = PdfPages(outfile_dir+"/"+"fit_plots.pdf")
 
-		plt.subplot(224,aspect ='equal')
-		plt.plot(fine['I'][:,i],fine['Q'][:,i],'o')
-		plt.plot(gain['I'][:,i],gain['Q'][:,i],'o')
-		plt.xlabel("I")
-		plt.ylabel("Q")
-		plt.xlim(np.min(fine['I'][:,i]),np.max(fine['I'][:,i]))
-		plt.ylim(np.min(fine['Q'][:,i]),np.max(fine['Q'][:,i]))
+    all_fits_mag = np.zeros((8,fine['freqs'].shape[1]))
+    all_fits_iq = np.zeros((9,fine['freqs'].shape[1]))
 
-		# fit nonlinear iq 
-		try:
-			x0 = resonance_fitting.guess_x0_iq_nonlinear_sep(fine_f,fine_z,gain_f,gain_z,verbose = True)
-			fit_dict_iq = resonance_fitting.fit_nonlinear_iq_sep(fine_f,fine_z,gain_f,gain_z,x0=x0)
-			all_fits_iq[:,i] = fit_dict_iq['fit'][0]
-			plt.subplot(222,aspect ='equal')
-			plt.plot(np.real(fit_dict_iq['fit_result']),np.imag(fit_dict_iq['fit_result']),"+")
-			plt.plot(np.real(fit_dict_iq['x0_result']),np.imag(fit_dict_iq['x0_result']),"x")
-			plt.subplot(224,aspect ='equal')
-			plt.plot(np.real(fit_dict_iq['fit_result']),np.imag(fit_dict_iq['fit_result']),"+")
-			plt.plot(np.real(fit_dict_iq['x0_result']),np.imag(fit_dict_iq['x0_result']),"x")
-			plt.plot(fine['I'][:,i],fine['Q'][:,i])
-		except Exception as e:
-			print(e)
-			print("could not fit the resonator")
+    for i in range(0,fine['freqs'].shape[1]):
+        fine_f = fine['freqs'][:,i]*10**6
+        gain_f = gain['freqs'][:,i]*10**6
+        fine_z = fine['I'][:,i]+1.j*fine['Q'][:,i]
+        gain_z = gain['I'][:,i]+1.j*gain['Q'][:,i]
+
+        #flag data that is too close to other resonators              
+        distance = center_freqs-center_freqs[i]
+        if center_freqs[i] != np.min(center_freqs): #don't do if lowest frequency resonator
+            closest_lower_dist = -np.min(np.abs(distance[np.where(distance<0)]))
+            closest_lower_index = np.where(distance ==closest_lower_dist)[0][0]
+            halfway_low = (center_freqs[i] + center_freqs[closest_lower_index])/2.
+        else:
+            halfway_low = 0
+
+        if center_freqs[i] != np.max(center_freqs): #don't do if highest frequenct
+            closest_higher_dist = np.min(np.abs(distance[np.where(distance>0)]))
+            closest_higher_index = np.where(distance ==closest_higher_dist)[0][0]
+            halfway_high = (center_freqs[i] + center_freqs[closest_higher_index])/2.
+        else:
+            halfway_high = np.inf
+           
+        use_index = np.where(((fine_f/10**6>halfway_low) & (fine_f/10**6<halfway_high)))
+        fine_f = fine_f[use_index]
+        fine_z = fine_z[use_index]
+
+        #flag gain data that is to close to all of the resonators
+        fine_span = (fine_f[-1]-fine_f[0])/10**6
+        use_boolean = np.zeros(len(gain_f))
+        for j in range(0,len(gain_f)):
+            if (np.abs(gain_f[j]/10**6-center_freqs)< fine_span/2.).any():
+                use_boolean[j] = 1
+
+        use_index_gain = np.where(use_boolean != 1)
+        if len(use_index_gain[0]>1):
+            gain_f = gain_f[use_index_gain]
+            gain_z = gain_z[use_index_gain]
+
+
+        fig = plt.figure(i,figsize = (12,12))
+
+        plt.subplot(221)
+        plt.title("Resonator Index "+str(i))
+        plt.plot(fine['freqs'][:,i],10*np.log10(fine['I'][:,i]**2+fine['Q'][:,i]**2),'o',label = "gain")
+        plt.plot(gain['freqs'][:,i],10*np.log10(gain['I'][:,i]**2+gain['Q'][:,i]**2),'o',label = "fine")
+        plt.xlabel("Frequency (MHz)")
+        plt.ylabel("Power (dB)")
+
+        plt.subplot(223)
+        plt.plot(fine['freqs'][:,i],10*np.log10(fine['I'][:,i]**2+fine['Q'][:,i]**2),'o')
+        plt.plot(gain['freqs'][:,i],10*np.log10(gain['I'][:,i]**2+gain['Q'][:,i]**2),'o')
+        plt.xlabel("Frequency (MHz)")
+        plt.ylabel("Power (dB)")
+        plt.xlim(np.min(fine['freqs'][:,i]),np.max(fine['freqs'][:,i]))
+
+        # fit nonlinear magnitude
+        try:
+            x0 = resonance_fitting.guess_x0_mag_nonlinear_sep(fine_f,fine_z,gain_f,gain_z,verbose = True)
+            fit_dict_mag = resonance_fitting.fit_nonlinear_mag_sep(fine_f,fine_z,gain_f,gain_z,x0=x0)#,bounds =bounds)
+            all_fits_mag[:,i] = fit_dict_mag['fit'][0]
+            plt.subplot(221)
+            plt.plot(fit_dict_mag['fit_freqs']/10**6,10*np.log10(fit_dict_mag['fit_result']),"+",label = "fit")
+            plt.plot(fit_dict_mag['fit_freqs']/10**6,10*np.log10(fit_dict_mag['x0_result']),"x",label = "x0 guess")
+            plt.legend()
+            plt.subplot(223)
+            plt.plot(fit_dict_mag['fit_freqs']/10**6,10*np.log10(fit_dict_mag['fit_result']),"+")
+            plt.plot(fit_dict_mag['fit_freqs']/10**6,10*np.log10(fit_dict_mag['x0_result']),"x")
+        except Exception as e:
+            print(e)
+            print("could not fit the resonator")
+
+
+        plt.subplot(222,aspect ='equal')
+        plt.plot(fine['I'][:,i],fine['Q'][:,i],'o')
+        plt.plot(gain['I'][:,i],gain['Q'][:,i],'o')
+        plt.xlabel("I")
+        plt.ylabel("Q")
+
+        plt.subplot(224,aspect ='equal')
+        plt.plot(fine['I'][:,i],fine['Q'][:,i],'o')
+        plt.plot(gain['I'][:,i],gain['Q'][:,i],'o')
+        plt.xlabel("I")
+        plt.ylabel("Q")
+        plt.xlim(np.min(fine['I'][:,i]),np.max(fine['I'][:,i]))
+        plt.ylim(np.min(fine['Q'][:,i]),np.max(fine['Q'][:,i]))
+
+        # fit nonlinear iq 
+        try:
+            x0 = resonance_fitting.guess_x0_iq_nonlinear_sep(fine_f,fine_z,gain_f,gain_z,verbose = True)
+            fit_dict_iq = resonance_fitting.fit_nonlinear_iq_sep(fine_f,fine_z,gain_f,gain_z,x0=x0)
+            all_fits_iq[:,i] = fit_dict_iq['fit'][0]
+            plt.subplot(222,aspect ='equal')
+            plt.plot(np.real(fit_dict_iq['fit_result']),np.imag(fit_dict_iq['fit_result']),"+")
+            plt.plot(np.real(fit_dict_iq['x0_result']),np.imag(fit_dict_iq['x0_result']),"x")
+            plt.subplot(224,aspect ='equal')
+            plt.plot(np.real(fit_dict_iq['fit_result']),np.imag(fit_dict_iq['fit_result']),"+")
+            plt.plot(np.real(fit_dict_iq['x0_result']),np.imag(fit_dict_iq['x0_result']),"x")
+            plt.plot(fine['I'][:,i],fine['Q'][:,i])
+        except Exception as e:
+            print(e)
+            print("could not fit the resonator")
 
 
 
 
 
 
-		pdf_pages.savefig(fig)
-		plt.close(fig)
+        pdf_pages.savefig(fig)
+        plt.close(fig)
 
-	pdf_pages.close()
+    pdf_pages.close()
 
-	pdf_pages = PdfPages(outfile_dir+"/"+"fit_results.pdf")
+    pdf_pages = PdfPages(outfile_dir+"/"+"fit_results.pdf")
 
-	fig = plt.figure(1, figsize = (12,6))
-	plt.title("Center frequency")
-	plt.plot(all_fits_mag[0,:]/10**6,'o',label = "Mag fit")
-	plt.plot(all_fits_iq[0,:]/10**6,'o',label = "IQ fit")
-	plt.xlabel("resonator index")
-	plt.ylabel("Resonator Frequency (MHz)")
-	plt.legend(loc = 4)
-	pdf_pages.savefig(fig)
-	plt.close()
+    fig = plt.figure(1, figsize = (12,6))
+    plt.title("Center frequency")
+    plt.plot(all_fits_mag[0,:]/10**6,'o',label = "Mag fit")
+    plt.plot(all_fits_iq[0,:]/10**6,'o',label = "IQ fit")
+    plt.xlabel("resonator index")
+    plt.ylabel("Resonator Frequency (MHz)")
+    plt.legend(loc = 4)
+    pdf_pages.savefig(fig)
+    plt.close()
 
-	fig = plt.figure(2,figsize = (12,6))
-	plt.title("Resonator Qs")
-	plt.plot(all_fits_mag[1,:],'o',label = "Qr Mag",color = 'g')
-	plt.plot(all_fits_iq[1,:],'*',label = "Qr IQ",color = 'g')
-	plt.plot(all_fits_mag[1,:]/all_fits_mag[2,:],'o',label = "Qc Mag",color = 'b')
-	plt.plot(all_fits_iq[1,:]/all_fits_mag[2,:],'*',label = "Qc IQ",color = 'b')
-	plt.plot(1/(1/all_fits_mag[1,:]-1/(all_fits_mag[1,:]/all_fits_mag[2,:])),'o',label = "Qi Mag",color = 'r')
-	plt.plot(1/(1/all_fits_iq[1,:]-1/(all_fits_iq[1,:]/all_fits_iq[2,:])),'*',label = "Qi IQ",color = 'r')
-	plt.xlabel("Resonator index")
-	plt.ylabel("Resonator Q")
-	plt.yscale('log')
-	plt.legend()
-	pdf_pages.savefig(fig)
-	plt.close()
+    fig = plt.figure(2,figsize = (12,6))
+    plt.title("Resonator Qs")
+    plt.plot(all_fits_mag[1,:],'o',label = "Qr Mag",color = 'g')
+    plt.plot(all_fits_iq[1,:],'*',label = "Qr IQ",color = 'g')
+    plt.plot(all_fits_mag[1,:]/all_fits_mag[2,:],'o',label = "Qc Mag",color = 'b')
+    plt.plot(all_fits_iq[1,:]/all_fits_mag[2,:],'*',label = "Qc IQ",color = 'b')
+    plt.plot(1/(1/all_fits_mag[1,:]-1/(all_fits_mag[1,:]/all_fits_mag[2,:])),'o',label = "Qi Mag",color = 'r')
+    plt.plot(1/(1/all_fits_iq[1,:]-1/(all_fits_iq[1,:]/all_fits_iq[2,:])),'*',label = "Qi IQ",color = 'r')
+    plt.xlabel("Resonator index")
+    plt.ylabel("Resonator Q")
+    plt.yscale('log')
+    plt.legend()
+    pdf_pages.savefig(fig)
+    plt.close()
 
-	fig = plt.figure(3,figsize = (12,6))
-	plt.title("Non linearity parameter a")
-	plt.plot(all_fits_mag[4,:],'o',label = "a Mag")
-	plt.plot(all_fits_iq[4,:],'o',label = "a IQ")
-	plt.xlabel("Resonator index")
-	plt.ylabel("Non-linearity parameter a")
-	plt.ylim(0,1)
-	plt.legend()
-	pdf_pages.savefig(fig)
-	plt.close()
+    fig = plt.figure(3,figsize = (12,6))
+    plt.title("Non linearity parameter a")
+    plt.plot(all_fits_mag[4,:],'o',label = "a Mag")
+    plt.plot(all_fits_iq[4,:],'o',label = "a IQ")
+    plt.xlabel("Resonator index")
+    plt.ylabel("Non-linearity parameter a")
+    plt.ylim(0,1)
+    plt.legend()
+    pdf_pages.savefig(fig)
+    plt.close()
 
-	pdf_pages.close()
+    pdf_pages.close()
 
-	np.save(outfile_dir+"/"+"all_fits_mag",all_fits_mag)
-	np.save(outfile_dir+"/"+"all_fits_iq",all_fits_iq)
-	np.savetxt(outfile_dir+"/"+"all_fits_mag.csv",all_fits_mag,delimiter = ',')
-	np.savetxt(outfile_dir+"/"+"all_fits_iq.csv",all_fits_iq,delimiter = ',')
+    np.save(outfile_dir+"/"+"all_fits_mag",all_fits_mag)
+    np.save(outfile_dir+"/"+"all_fits_iq",all_fits_iq)
+    np.savetxt(outfile_dir+"/"+"all_fits_mag.csv",all_fits_mag,delimiter = ',')
+    np.savetxt(outfile_dir+"/"+"all_fits_iq.csv",all_fits_iq,delimiter = ',')
 
 
 def calibrate_multi(fine_filename,gain_filename,stream_filename,skip_beginning = 0,plot_period = 10,bin_num = 1,outfile_dir = "./",sample_rate = 488.28125):
