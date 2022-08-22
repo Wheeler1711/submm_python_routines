d4f37cdc17108b49f31ffe3d0ec814b2eab8010f
Wheeler <jdw8@687jwmbp.local>
2022-08-22 11:37:47 -0600

    File to allow you to call the res fit demo from python terminal
    after pip installing or python setup.py install run:
    from submm.demo import res_fit_demo
    res_fit_demo.run()
    
    This will install jupyterlab if you don't already have jupyter

---
+++ submm/demo/res_fit_demo.py
@@ -0,0 +1,19 @@
+import os
+import sys
+dir_this_file = os.path.dirname(os.path.realpath(__file__))
+dir_notebook = os.path.join(dir_this_file, "res_fit.ipynb")
+print("demo notebook located at")
+print(dir_notebook)
+
+try:
+    import jupyterlab
+except:
+    input_str = input("This example requires a jupyterlab notebooks. Would you like to install that (y/n)?")
+    if input_str.lower() == 'y':
+        os.system("pip install jupyterlab")
+    else:
+        sys.exit("Exiting")
+
+def run():
+    os.system("jupyter notebook "+dir_notebook)
+
