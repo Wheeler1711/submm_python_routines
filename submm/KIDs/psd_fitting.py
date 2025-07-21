1e048d52a170b6a42b41ffb477462cfe77440d6b
Wheeler1711 <Wheeler1711@gmail.com>
2025-07-21 09:52:44 -0600

    added in new fitter for PSDs that accounts for TLS noise time constant
    TLS noise is not affected by the quasipartical time constant only
    the resonator ring time constant and thus the model needs to be
    different that assuming a 1/f component that rolls off with the
    quasipartical time constant

--- submm/KIDs/psd_fitting.py
+++ submm/KIDs/psd_fitting.py
@@ -20,11 +20,19 @@ def noise_slope(y,b,c):
     return b*y**-c
 def noise_white(y,a):
     return np.ones(y.shape[0])*a
+def noise_white_rolloff(y,a,d):
+    return (a)/(1+(2*np.pi*y*d)**2.)
 def noise_profile_lor(y,a,b,c,d):
     return (a+b*y**-c)/(1+(2*np.pi*y*d)**2.)
 def noise_profile_lor_vec(y,a,b,c,d):
     y = np.reshape(y,(y.shape[0],1,1,1,1))
     return (a+b*y**-c)/(1+(2*np.pi*y*d)**2.)
+def noise_profile_lor_TLS(y,a,b,c,d,e):
+    return (a/(1+(2*np.pi*y*d)**2.)+b*y**-c)*(1/(1+(2*np.pi*y*e)**2.))
+def noise_profile_lor_TLS_vec(y,a,b,c,d,e):
+    y = np.reshape(y,(y.shape[0],1,1,1,1,1))
+    return (a/(1+(2*np.pi*y*d)**2.)+b*y**-c)*(1/(1+(2*np.pi*y*e)**2.))
+
 
 def std_of_mean(x):
     if np.size(x) == 1:
@@ -252,7 +260,7 @@ def fit_psd_lor_brute(x, y, n_grid_points=20, error=None, **keywords):
                                  index_for_time_constant]))]])  # look for 3dB decrease from white noise guess
         print("guess values are")
         print(x0_guess)
-        ranges = np.asarray(([x0_guess[0] / 2, x0_guess[1] / 20, 0.4, x0_guess[3] / 2],
+        ranges = np.asarray(([x0_guess[0] / 2, x0_guess[1] / 80, 0.4, x0_guess[3] / 2],
                              [x0_guess[0] * 2, x0_guess[1] * 10, 2, x0_guess[3] * 2]))
 
     if error is None:
@@ -331,6 +339,166 @@ def fit_psd_lor_brute(x, y, n_grid_points=20, error=None, **keywords):
     fit_dict = {'fit_values': fit_values, 'fit_values_names': fit_values_names, 'sum_dev': sum_dev,
                 'fit_result': fit_result, 'x0_guess_result': x0_guess_result, 'evaluated_ranges': evaluated_ranges,
                 'knee': knee,
+                'noise_slope_result': noise_slope_result}  # , 'x0':x0, 'z':z},'marginalized_2d':marginalized_2d,'marginalized_1d':marginalized_1d,
+    return fit_dict
+
+
+def fit_psd_lor_TLS_brute(x, y, tau_Q,n_grid_points=20, error=None, **keywords):
+    """
+    brute force fitting (the only way to fit with 4 or less variables)
+    x is the psd frequency
+    y is the psd magnitude
+    n_grid_points is the deminsion of the 4 dimensional cube over which function will be evaluated
+    keywords are
+    ranges is the ranges for each parameter i.e. np.asarray(([a_low,b_low,c_low,d_low],[a_high,b_high,c_high,d_high]))
+    freq_range = (f_low,f_high) bounds over which the psd should be fit
+    white_freq = 50 (i.e) Hz if using the automated range it is very useful to specify where the noise psd is white (frequency independant)
+    use_range ---is an n length tuple of frequencies to use while fitting
+    Example: [[1,57],[63,117],[123,177]] here we fit from 1 to 57Hz and 63 to 117 Hz and 123 to 177Hz avoid 60 Hz and harmonic
+    slope: looking for TLS with a exponent of 0.5 the just force slope to be slope = 0.5
+
+    due to the vector nature of the calculations used by this brute for fitter
+    n_grid_points will be limited by your computers ram and it grows fast
+    for example 50^4*(2bytes per float*(2arrays) = 3.5GB of ram
+    just watch your resources when you fit if you exceed your ram you will write to disk and the fit will never finish
+
+    To Do add in marginilaztions for error bars like in the brute force fitter in resonance fitting
+    also add in the corner plot for marginalized values
+    would be good to add in a nested version of this where it fits again over a smaller parameter space
+    -Done for slope should think about making it general
+    """
+
+    if ('slope' in keywords):
+        slope = keywords['slope']
+    else:
+        slope = None
+
+    if ('freq_range' in keywords):
+        freq_range = keywords['freq_range']
+    else:
+        freq_range = (x[0], x[-1])
+
+    if ('use_range' in keywords):
+        use_range = keywords['use_range']
+        # create an index of the values you want to fit
+        index_for_fitting = np.where((x>use_range[0][0]) & (x<use_range[0][1]) )[0]
+        for i in range(1,len(use_range)):
+            index2 = np.where((x>use_range[i][0]) & (x<use_range[i][1]) )
+            index_for_fitting = np.hstack((index_for_fitting,index2[0]))  
+    else:
+        index_for_fitting = np.where(((x >= freq_range[0]) & (x <= freq_range[1])))
+ 
+
+    if ('ranges' in keywords):
+        ranges = keywords['ranges']
+    else:
+        if ('white_freq' in keywords):
+            white_freq = keywords['white_freq']
+            white_index = np.argmin(np.abs(x[index_for_fitting] - white_freq))
+        else:
+            white_index = len(y[index_for_fitting]) // 2  # noise is white in the middle of psd
+
+        # only look at frequencies greater than white noise frequency
+        index_for_time_constant = np.where(x >= x[index_for_fitting][white_index])
+
+        white_guess = y[index_for_fitting][white_index]
+        # assume 1/f dominates at lowest frequency but still subtract off white noise
+        one_over_f_guess = (y[index_for_fitting][0] - white_guess) / x[index_for_fitting][0] ** (-1.)
+        if one_over_f_guess<0: #unpysical to be less than 0 must be small though
+            one_over_f_guess = white_guess/10.
+        x0_guess = np.array([white_guess,
+                             one_over_f_guess,
+                             # assume 1/f dominates at lowest frequency but still subtract off white noise
+                             1,  # guess 1/f is index is 1
+                             1. / 2 / np.pi / x[index_for_time_constant][np.argmin(np.abs(white_guess / 2. - y[
+                                 index_for_time_constant]))]])  # look for 3dB decrease from white noise guess
+        print("guess values are")
+        print(x0_guess)
+        ranges = np.asarray(([x0_guess[0] / 2, x0_guess[1] / 80, 0.4, x0_guess[3] / 2],
+                             [x0_guess[0] * 2, x0_guess[1] * 10, 2, x0_guess[3] * 2]))
+
+    if error is None:
+        error = np.ones(len(x[index_for_fitting]))
+      
+    if not slope:
+        a_values = np.linspace(ranges[0][0], ranges[1][0], n_grid_points)
+        b_values = np.linspace(ranges[0][1], ranges[1][1], n_grid_points)
+        c_values = np.linspace(ranges[0][2], ranges[1][2], n_grid_points)
+        d_values = np.linspace(ranges[0][3], ranges[1][3], n_grid_points)
+        evaluated_ranges = np.vstack((a_values, b_values, c_values, d_values))
+
+        a, b, c, d = np.meshgrid(a_values, b_values, c_values, d_values, indexing="ij")  # always index ij
+        noise_profile_fixed_ringdown = lambda x_lamb,a,b,c,d,e: noise_profile_lor_TLS_vec(x_lamb,a,b,c,d,tau_Q)
+
+        #evaluated = noise_profile_lor_vec(x[index_for_fitting], a, b, c, d)
+        evaluated = noise_profile_fixed_ringdown(x[index_for_fitting],a,b,c,d,tau_Q)
+        evaluated = evaluated[:,0,:,:,:,:]
+        
+        data_values = np.reshape(y[index_for_fitting], (y[index_for_fitting].shape[0], 1, 1, 1, 1))
+        error = np.reshape(error, (y[index_for_fitting].shape[0], 1, 1, 1, 1))
+        print(evaluated.shape)
+        print(data_values.shape)
+        print(error.shape)
+        sum_dev = np.sum(((evaluated - data_values) ** 2 / error ** 2),
+                         axis=0)  # comparing in magnitude space rather than magnitude squared
+        # print(sum_dev.shape)
+
+        min_index = np.where(sum_dev == np.min(sum_dev))
+        print("grid values at minimum are")
+        print(min_index)
+        index1 = min_index[0][0]
+        index2 = min_index[1][0]
+        index3 = min_index[2][0]
+        index4 = min_index[3][0]
+
+        fit_values = np.asarray((a_values[index1], b_values[index2], c_values[index3], d_values[index4],tau_Q))
+    else:
+        a_values = np.linspace(ranges[0][0], ranges[1][0], n_grid_points)
+        b_values = np.linspace(ranges[0][1], ranges[1][1], n_grid_points)
+        #c_values = np.linspace(ranges[0][2], ranges[1][2], n_grid_points)
+        d_values = np.linspace(ranges[0][3], ranges[1][3], n_grid_points)
+        evaluated_ranges = np.vstack((a_values, b_values, d_values))
+
+        a, b, d = np.meshgrid(a_values, b_values, d_values, indexing="ij")  # always index ij
+
+        noise_profile_fixed_slope = lambda x_lamb,a,b,c,d,e: noise_profile_lor_TLS_vec(x_lamb,a,b,slope,d,tau_Q)
+        evaluated = noise_profile_fixed_slope(x[index_for_fitting],a,b,slope,d,tau_Q)
+        #print(evaluated)
+        #print(evaluated.shape)
+        evaluated = evaluated[:,0,0,:,:,:]
+        data_values = np.reshape(y[index_for_fitting], (y[index_for_fitting].shape[0], 1, 1, 1))
+        error = np.reshape(error, (y[index_for_fitting].shape[0], 1, 1, 1))
+        
+        # print(evaluated.shape)
+        # print(data_values.shape)
+        # print(error.shape)
+        
+        sum_dev = np.sum(((evaluated - data_values) ** 2 / error ** 2),
+                         axis=0)  # comparing in magnitude space rather than magnitude squared
+        # print(sum_dev.shape)
+
+        min_index = np.where(sum_dev == np.min(sum_dev))
+        print("grid values at minimum are")
+        print(min_index)
+        index1 = min_index[0][0]
+        index2 = min_index[1][0]
+        #index3 = min_index[2][0]
+        index4 = min_index[2][0]
+        
+        fit_values = np.asarray((a_values[index1], b_values[index2], slope, d_values[index4],tau_Q))
+
+    fit_values_names = ('a (white)', 'b (1/f)', 'c (1/f exponent)', 'd (tau)','e (tau_Q)')
+    fit_result = noise_profile_lor_TLS(x, fit_values[0], fit_values[1], fit_values[2], fit_values[3],fit_values[4])
+    x0_guess_result = noise_profile_lor_TLS(x, x0_guess[0], x0_guess[1], x0_guess[2], x0_guess[3],tau_Q)
+    noise_slope_result = noise_slope(x, fit_values[1], fit_values[2])
+    noise_white_result = noise_white_rolloff(x, fit_values[0], fit_values[3])
+    fine_freqs = np.logspace(np.log10(freq_range[0]), np.log10(freq_range[1]), 10000)
+    #print(fine_freqs)
+    knee = fine_freqs[np.argmin(np.abs(fit_values[1] * fine_freqs ** -fit_values[2] - fit_values[0]))]
+
+    fit_dict = {'fit_values': fit_values, 'fit_values_names': fit_values_names, 'sum_dev': sum_dev,
+                'fit_result': fit_result, 'x0_guess_result': x0_guess_result, 'evaluated_ranges': evaluated_ranges,
+                'knee': knee,'noise_white_result': noise_white_result,
                 'noise_slope_result': noise_slope_result}  # , 'x0':x0, 'z':z},'marginalized_2d':marginalized_2d,'marginalized_1d':marginalized_1d,
     return fit_dict
 
